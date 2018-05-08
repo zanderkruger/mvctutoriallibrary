cb94152d70529e6a2b68cdc134ef93d4d79b95c3
zander.kruger <zander.kruger@entelect.co.za>
2018-05-08 12:20:14 +0200

    Added Library Context

--- LibraryData/LibraryContext.cs
+++ LibraryData/LibraryContext.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryData
 {
     public class LibraryContext
     {
-        
+        public LibraryContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Patron> Patrons {get; set}
+
     }
 }
