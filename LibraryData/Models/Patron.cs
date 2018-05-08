fbb476a39dc1435f0f732c531ec29a0596342dfa
zander.kruger <zander.kruger@entelect.co.za>
2018-05-08 12:04:34 +0200

    added dependencies for database interaction, added model for Patron, added model layer

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron{
+        public int Id {get; set;}
+        public string firstName {get; set;}
+        public string lastName {get; set;}
+
+    }
+}
