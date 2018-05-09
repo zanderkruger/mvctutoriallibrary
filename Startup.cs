470ae011e766f53d04b416a49c8d75308835d342
zander.kruger <zander.kruger@entelect.co.za>
2018-05-09 09:43:12 +0200

    Added more necessary dependencies, fixed issue with duplicate package declarations, trying to get add migration to work

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace mvctutoriallibrary
         {
             services.AddMvc();
 
-            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")))
+            services.AddDbContext<LibraryContext>((options) => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
